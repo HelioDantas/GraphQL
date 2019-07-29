73750fd0a3aac598f7ca260b50f8e942d164ff27
Helio Dantas <helio_dantas@outlook.com>
2019-07-29 10:39:41 -0300

    metodo teste para vertice fora da raiz Signed-off-by: Helio Dantas <helio_dantas@outlook.com>

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,8 +1,12 @@
 const user = require('./user');
+const todo = [{id: 312312, content: 'sdfsdfdsfsd'}, {id: 312312, content: 'sdfsdfdsfsd'}];
 module.exports = {
     Query: {
         users: () => user.find(),
-        user: (_, {id}) => user.findById(id)
+        user: (_, {id}) => user.findById(id),
+    },
+    User: {
+        todos: (_, {id}) => todo
     },
     Mutation: {
         user: (_, {name, email}) => user.create({name, email})
